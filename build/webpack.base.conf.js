307e5dca59d7c6ab3dd9161f7535b83852beb72d
mars <578595193@qq.com>
2017-06-16 23:41:15 +0800

    config image path

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,8 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'assets': path.resolve(__dirname, '../src/assets')
     }
   },
   module: {
